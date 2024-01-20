72db96c72ccf61fd5e0d0259f6144219a0ec3de1
batuhanbaybas <b.baybas@gmail.com>
2024-01-20 19:59:28 +0300

    Refactor auth controller imports

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthUserDto } from './dto/auth.dto';
 
