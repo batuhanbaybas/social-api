91ba552e04325f6971db7044a85e06cdaece0952
batuhanbaybas <b.baybas@gmail.com>
2024-01-22 23:52:11 +0300

    Remove FriendModule and FrinedService imports and providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,6 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { FeedModule } from './feed/feed.module';
 import { CommentModule } from './comment/comment.module';
-import { FriendModule } from './friend/friend.module';
-import { FrinedService } from './frined/frined.service';
 
 @Module({
   imports: [
@@ -18,8 +16,6 @@ import { FrinedService } from './frined/frined.service';
     UserModule,
     FeedModule,
     CommentModule,
-    FriendModule,
   ],
-  providers: [FrinedService],
 })
 export class AppModule {}
